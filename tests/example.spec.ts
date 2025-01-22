e93a588b111dfbcba2687cc54d9359f6fe028a17
ArabindaAutomation <arabindam748@gmail.com>
2025-01-22 19:24:24 +0530

    added account page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -26,3 +26,12 @@ test('homepagetest', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+test('accountpagetest', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link  added comment Arabinda.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
