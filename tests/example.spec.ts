109d07bc89953a8a5a06599bd02dbfa4dec746c8
ArabindaAutomation <arabindam748@gmail.com>
2025-01-22 19:04:16 +0530

    fix: update example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,13 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('homepagetest', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link  added comment Arabinda.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
